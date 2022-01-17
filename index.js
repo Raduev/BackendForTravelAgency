b6da15232934857325322a8535db7ce635ba49e2
Raduev <dominator95rrr@gmail.com>
2022-01-17 15:30:09 +0300

    Repository has been created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const app = express()
+const port = 4000
+require("dotenv").config()
+
+app.use(cors())
+app.use(express.json())
+app.use(require("./routes/index"))
+
+const connect = async () => {
+    await mongoose.connect(
+        "mongodb+srv://tamerlan:raduev@cluster0.14x5g.mongodb.net/TravelAgency"
+    )
+    console.log("Соединение успешно установлено")
+
+    app.listen(port, () => {
+        console.log(`Server has been started on port http://localhost:${port}`)
+    })
+}
+
+connect()
---
+++ models/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+const jwt  = require('jsonwebtoken')
+module.exports = async (req, res, next) => {
+
+  const { authorization } = req.headers
+
+  if(!authorization) {
+    return res.status(401).json('Нет авторизации')
+  }
+  const [type, token] = authorization.split(' ');
+
+  if (type !== "Bearer") {
+    return res.status(400).json('Неверный тип токена')
+  }
+
+  try {
+    req.user = await jwt.verify(token, process.env.SECRET_JWT_KEY)
+
+
+    next();
+
+  }
+  catch (e) {
+    res.status(401).json('Ошибка авторизации' + e.toString())
+  }
+}
