29a4b21911a5a9338fe066da17c628764d9cb579
Mamdi <masuev12@gmail.com>
2022-01-18 12:52:30 +0300

    Авторизация и регистрация пользователя

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
