d71bb2f4d1274762a1eb06d72dc318fe6cc5ab1a
halid <barzaev97@gmail.com>
2022-01-18 16:33:56 +0300

    add img

--- routes/room.route.js
+++ routes/room.route.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const upload = require('../middleware/upload.middlware')
 const {roomController} = require('../controllers/room.controller')
 const router = Router()
 
@@ -6,7 +7,7 @@ router.get('/getRooms', roomController.getRooms )
 router.post('/addRooms', roomController.addRoom )
 router.delete('/removeRoom/:id', roomController.removeRoom )
 router.patch('/patchRoom/:id', roomController.patchRoom )
-
+router.patch('/rooms/:id/avatar', upload.single('img'), roomController.updateImg)
 
 module.exports = router
 
