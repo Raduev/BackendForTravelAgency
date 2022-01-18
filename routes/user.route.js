22124f659b03dfb90c8aff2a00f38bbcdece6c5a
Mamdi <masuev12@gmail.com>
2022-01-18 13:29:55 +0300

    Добавление мидлвера в роут

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const { usersController } = require("../controllers/users.controller");
 const { Router } = require("express");
+const auth = require('../middleware/auth.middleware')
 
 const router = Router();
 
 router.get("/users", usersController.getAllUsers);
-router.get("/users/:id", usersController.getUserById);
+router.get("/users/:id",auth, usersController.getUserById);
 router.post("/users", usersController.registerUser);
 router.post("/login", usersController.login);
 router.post('/admin', usersController.postAdmin)
