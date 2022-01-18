46a18a39e5ca01f0a5930987897663169883ca53
Raduev <dominator95rrr@gmail.com>
2022-01-18 14:00:18 +0300

    fix

--- models/Tur.model.js
+++ models/Tur.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const turSchema = mongoose.Schema({
   from: { type: String, required: true },
   to: { type: String, required: true },
-  data: { type: Number, required: true },
+  data: { type: String, required: true },
   night: { type: Number, required: true },
   amount: { type: Number, required: true },
   hotel: { type: mongoose.SchemaTypes.ObjectId, ref: "Hotel" },
