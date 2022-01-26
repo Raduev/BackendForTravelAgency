d88e025f868f7d65d20b91bec14a682d8379319a
Ibragim <mih230903@gmail.com>
2022-01-26 10:23:50 +0300

    изменил роут юзера

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth.middleware')
 const router = Router();
 
 router.get("/users", usersController.getAllUsers);
-router.get("/users/:id",auth, usersController.getUserById);
+router.get("/user",auth, usersController.getUserById);
 router.post("/users", usersController.registerUser);
 router.post("/login", usersController.login);
 router.post('/admin', usersController.postAdmin)
